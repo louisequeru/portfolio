e7aa2906705ff91625fe5e1c1728b2e9ccc61d41
louisequeru <louise.queru@studentfr.ch>
2025-04-16 12:20:30 +0200

    finalisation du scroll horizontal, amélioration de l'animation de l'étoile

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,42 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+// var tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".star-start",
+//     markers: true,
+//     scrub: 1,
+//   },
+// });
+
+// tl.to(".star", {
+//   x: 400,
+//   scale: 0.8,
+//   rotation: 45,
+//   duration: 1,
+// });
+
+// tl.to(".star", {
+//   x: -400,
+//   scale: 0.8,
+//   rotation: -45,
+//   duration: 1,
+// });
+
+gsap.to(".star", {
+  scrollTrigger: {
+    trigger: ".star",
+    markers: true,
+    pin: ".star",
+    start: "top",
+    end: "bottom",
+  },
+});
+
 gsap.to(".star", {
   scrollTrigger: {
     trigger: ".star-start",
-    markers: true,
+    // markers: true,
     scrub: 1,
   },
   x: 400,
@@ -15,14 +47,36 @@ gsap.to(".star", {
   duration: 1,
 });
 
-gsap.to(".star", {
-  scrollTrigger: {
-    trigger: ".star-end",
-    markers: true,
-    scrub: 1,
+gsap.fromTo(
+  ".star",
+  {
+    x: 400,
+    scale: 0.8,
+    rotation: 45,
   },
-  x: -400,
-  scale: 0.7,
-  rotation: -45,
-  duration: 1,
+  {
+    scrollTrigger: {
+      trigger: ".star-end",
+      // markers: true,
+      scrub: 1,
+    },
+    x: -400,
+    scale: 0.7,
+    rotation: -45,
+    duration: 1,
+  }
+);
+
+const sections = document.querySelectorAll(".projects-gd-img");
+const container = document.querySelector(".projects-gd");
+
+gsap.to(container, {
+  xPercent: ((sections.length - 1) / sections.length) * -100,
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    pin: true,
+    scrub: 1,
+    end: () => "+=" + container.offsetWidth,
+  },
 });
